b96e282fc85bc9ee53ce6c162aad7f809523d3e3
OmarAshrafTaha <omarashraftaha19@gmail.com>
2018-03-19 01:41:26 +0200

    modified:   CMakeLists.txt 	new file:   Multiplot_config/odom_imu.xml 	new file:   launch/arl_imu_odometry.launch 	modified:   package.xml 	modified:   scripts/imu.py 	renamed:    scripts/publish_example.py -> scripts/imu_example.py 	new file:   scripts/rqt.py

--- scripts/imu.py
+++ scripts/imu.py
@@ -10,7 +10,7 @@ from   math              import sin, cos, pi , radians , degrees
 from   std_msgs.msg      import Float32MultiArray
 from   geometry_msgs.msg import Point, Pose, Quaternion, Twist, Vector3
 
-rospy.init_node('arl_imu_odometry')
+rospy.init_node('igp_imu_odometry')
 odom_pub         = rospy.Publisher("odom_imu", Odometry, queue_size=50)
 imu_pub          = rospy.Publisher("Imu",Imu, queue_size=50)
 odom_broadcaster = tf.TransformBroadcaster()
@@ -18,9 +18,10 @@ odom_broadcaster = tf.TransformBroadcaster()
 s          = 0.0
 x          = 0.0
 y          = 0.0
+dt	   = 0.2
 th         = 0.0
-vx         = 0.0
-vy         = 0.0
+vx_raw     = 0.0
+vy_raw     = 0.0
 vth        = 0.0
 quat_w     = 0.0
 quat_x     = 0.0
@@ -34,7 +35,16 @@ acc_z_raw  = 0.0
 roll_rate  = 0.0
 pitch_rate = 0.0
 yaw_rate   = 0.0
-
+P_new	   = np.matrix(([0,0],[0,0]))
+P_old      = np.matrix(([0,0],[0,0]))
+Pv_new     = np.matrix(([0,0],[0,0]))
+Pv_old	   = np.matrix(([0,0],[0,0]))
+Fx         = np.matrix(([1,0],[0,1]))
+Fv         = np.matrix(([1,0],[0,1]))
+Fu 	   = np.matrix(([dt,0],[0,dt]))
+Q          = np.matrix(([(0.019*0.019),0],[0,(0.019*0.019)]))
+Pose_cov   = np.array([0.0]*36)
+Twist_cov  = np.array([0.0]*36)
 
 def listener():
     rospy.Subscriber('imu_data', Float32MultiArray , callback)
@@ -64,27 +74,30 @@ def callback(data):
 
 r = rospy.Rate(5.0) 
 
+ 
 while not rospy.is_shutdown():
     current_time = rospy.Time.now()
     listener()
                            #####################
                            # nav_msgs.msg/odom #
-    ###################################################################  
-    dt      = 0.2 
+    ###################################################################   
     th      = init_yaw-yaw
     th      = radians(th)
-    rot_mat = np.matrix(([cos(th),-1*sin(th)],[sin(th),cos(th)]))
-    acc_mat = np.matrix(([acc_x_raw],[acc_y_raw]))
-    acc_mat = rot_mat*acc_mat	 
-    acc_x   = float(acc_mat[0][0])
-    acc_y   = float(acc_mat[1][0])
-    vx      = vx + (acc_x*dt) 
-    vy      = vy + (acc_y*dt)
+    
+    rot_mat = np.matrix(([cos(th),-1*sin(th)],[sin(th),cos(th)]))   
+ 	 
+    vx_raw  = vx_raw + (acc_x_raw*dt) 
+    vy_raw  = vy_raw + (acc_y_raw*dt)
+    
+    v_mat   = np.matrix(([vx_raw],[vy_raw]))
+    v_mat   = rot_mat*v_mat
+    
+    vx      = v_mat[0,0]
+    vy      = v_mat[1,0]
     vth     = yaw_rate    
-    delta_x = (vx * cos(th) - vy * sin(th)) * dt
-    delta_y = (vx * sin(th) + vy * cos(th)) * dt
-    x      += delta_x
-    y      += delta_y
+    
+    x      += (vx*dt)
+    y      += (vy*dt)
     
     odom_quat = tf.transformations.quaternion_from_euler(0, 0, th)
     odom_broadcaster.sendTransform(
@@ -94,35 +107,33 @@ while not rospy.is_shutdown():
         "base_link",
         "odom"
         )
+    
+    if acc_x_raw != 0 :
+	    Pv_new = (Fv*Pv_old*Fv.T) + (Fu*Q*Fu.T)
+    
+    P_new  = (Fx*P_old*Fx.T)  + (Fv*Pv_new*Fu.T)
+    
+    Pose_cov[0]  = P_new[0,0]
+    Pose_cov[1]  = P_new[0,1]
+    Pose_cov[6]  = P_new[1,0]
+    Pose_cov[7]  = P_new[1,1]
+    
+    Twist_cov[0] = Pv_new[0,0]
+    Twist_cov[1] = Pv_new[0,1]
+    Twist_cov[6] = Pv_new[1,0]
+    Twist_cov[7] = Pv_new[1,1]
 
     odom = Odometry()
     odom.header.stamp    = current_time
     odom.header.frame_id = "odom"
-    odom.pose.pose       = Pose(Point(x, y, 0.), Quaternion(*odom_quat))
+    odom.pose.pose       = Pose(Point(x, y, 0.), Quaternion(*odom_quat)) 
+    odom.pose.covariance = Pose_cov
     odom.child_frame_id  = "base_link"
     odom.twist.twist     = Twist(Vector3(vx, vy, 0), Vector3(0, 0, vth))
+    odom.twist.covariance= Twist_cov
     odom_pub.publish(odom)
-    ###################################################################
     
-        
-                           ###################
-                           # sensor_msgs/Imu #
-    ###################################################################
-    imu = Imu()
-    imu.header.stamp    = current_time
-    imu.header.frame_id = "odom"    
-    imu.orientation.w = quat_w
-    imu.orientation.x = quat_x
-    imu.orientation.y = quat_y
-    imu.orientation.z = quat_z
-    imu.linear_acceleration.x = acc_x_raw
-    imu.linear_acceleration.y = acc_y_raw
-    imu.linear_acceleration.z = acc_z_raw
-    imu.linear_acceleration_covariance[0] = -1
-    imu.angular_velocity.x = roll_rate
-    imu.angular_velocity.y = pitch_rate
-    imu.angular_velocity.z = yaw_rate
-    imu.angular_velocity_covariance[0] = -1
-    imu_pub.publish(imu)    
-    ###################################################################
+    Pv_old = Pv_new
+    P_old  = P_new 
+    ###################################################################        
     r.sleep()
